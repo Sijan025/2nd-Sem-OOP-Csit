7dcd916bcbe5fc66816dc36d714a0ec3ec09f2aa
Sijan Bastola <201369766+Sijan025@users.noreply.github.com>
2026-09-06 09:00:47 +0545

    Add basic C++ program with Student class

---
+++ Chapter-02/basics_os_oop.cpp
@@ -0,0 +1,14 @@
+//Structure of C++ program 
+#include <iostream>
+using namespace std;
+class Student{
+public:
+  void show(){
+    cout<<"Hello Student";
+  }
+};
+int main(){
+  Student s1;
+  s1.show();
+return 0;
+}
