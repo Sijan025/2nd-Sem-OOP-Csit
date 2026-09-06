9f4fff5e96f821882a9b52f5533cd2eb322f312d
Sijan Bastola <201369766+Sijan025@users.noreply.github.com>
2026-09-06 08:48:17 +0545

    Add OOP concepts notes in program1.cpp
    Added notes on key OOP concepts including classes, objects, abstraction, encapsulation, inheritance, and polymorphism.

---
+++ Chapter-01/program1.cpp
@@ -0,0 +1,36 @@
+//Here are the some notes important i=from chapter 1 of OOP.
+//OOP are: Class, Object, abstraction, encapsulation, inheritance, and polymorphism.
+//Objects 
+// Objects are the physical instance created from the class
+// represents a real world entity and contains: state(Attribute/data) & Behavior(Methods/Functions)
+
+//Classes
+// blueprint or tempelete. defines structure & beheviour of something, but it is not the actual thing.
+// Class is a logical construct that exists in your code but does not occupy memory space for data values until it is used.
+
+// Data Abstraction : 
+// ➢ It is the process of hiding the internal details of how a class or method works and 
+// showing only the essential functionality to the user.
+// ➢ Objects only reveal internal mechanisms that are relevant for the use of other objects, 
+// hiding any unnecessary implementation code.
+// ➢ Focuses on what an object does, not how it does it
+
+// Encapsulation : 
+// ➢ Encapsulation involves bundling data and the methods
+// that operate on the data within a single unit called a "class” .
+// ➢ Binding (or wrapping) code and data together into a 
+// single unit are known as encapsulation.
+// ➢ Encapsulation acts as a protective shield around the data.
+// ➢ For example, a capsule, it is wrapped with different medicines
+
+// Inheritance : 
+// ➢ A programming technique that is used to reuse an existing class to build a new class is known as inheritance.
+// ➢ Inheritance is a mechanism where a child class (derived class) inherits attributes and methods from a parent class (base class).
+// ➢ The new class inherits all the behavior of the original class.
+// ➢ The inheritance relationship between the classes of a program is called a class hierarchy
+
+//Polymorphism : 
+// ➢ Polymorphism means “many forms”.
+// ➢ Polymorphism means the quality of having more than one form.
+// ➢ The representation of different behaviors using the same name is called polymorphism.
+// ➢ It allows the same operation/method to behave differently depending on the object or context
